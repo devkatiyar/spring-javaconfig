a604174d7f0273962c37055b6fb49aaad8cec8f0
Dev <Dev@DESKTOP-JDM7DO2.Home>
2017-04-19 23:41:02 +0100

    batch

---
+++ spring-javaconfig/com/model/Person.java
@@ -0,0 +1,55 @@
+package com.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PEOPLE")
+public class Person {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="person_id")
+	long person_id;
+	
+	@Column(name="first_name")
+	 private String firstName;
+    
+	
+	@Column(name="last_name")
+	private String lastName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "firstName: " + firstName + ", lastName: " + lastName;
+    }
+
+}
---
+++ spring-javaconfig/com/spring/BatchConfiguration.java
@@ -0,0 +1,104 @@
+package com.spring;
+
+import org.hibernate.SessionFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.database.HibernateItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
+
+import com.model.Person;
+import com.processor.JobCompletionNotificationListener;
+import com.processor.PersonItemProcessor;
+
+
+
+@Configuration
+@EnableBatchProcessing
+@Import(DataSourceConfiguration.class)
+public class BatchConfiguration {
+
+    @Autowired
+    public JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    public StepBuilderFactory stepBuilderFactory;
+    
+    
+    @Autowired
+    JobCompletionNotificationListener listener;
+
+ 
+    
+    
+    @Autowired
+	private SessionFactory sessionFactory;
+    
+    @Bean
+    public FlatFileItemReader<Person> reader() {
+        FlatFileItemReader<Person> reader = new FlatFileItemReader<Person>();
+        reader.setResource(new ClassPathResource("sample-data.csv"));
+        reader.setLineMapper(new DefaultLineMapper<Person>() {{
+            setLineTokenizer(new DelimitedLineTokenizer() {{
+                setNames(new String[] { "firstName", "lastName" });
+            }});
+            setFieldSetMapper(new BeanWrapperFieldSetMapper<Person>() {{
+                setTargetType(Person.class);
+            }});
+        }});
+        return reader;
+    }
+
+    @Bean
+    public PersonItemProcessor processor() {
+        return new PersonItemProcessor();
+    }
+
+    @Bean
+    public HibernateItemWriter<Person> writer() {  
+        HibernateItemWriter<Person> hibernateItemWriter=new HibernateItemWriter<Person>();
+        hibernateItemWriter.setSessionFactory(sessionFactory);
+        return hibernateItemWriter;
+    }
+    @Bean
+    public Job processOrderJob(JobCompletionNotificationListener listener) {
+        return jobBuilderFactory.get("importUserJob")
+                .incrementer(new RunIdIncrementer())
+                .listener(listener)
+                .flow(step1())
+                .end()
+                .build();
+    }
+
+    @Bean(name="executionSteps")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
+                .<Person, Person> chunk(10)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+    
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer() {
+//        TomcatEmbeddedServletContainerFactory factory = 
+//                      new TomcatEmbeddedServletContainerFactory();
+//        return factory;
+//     }
+    
+  
+}
